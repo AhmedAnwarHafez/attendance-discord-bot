a67f0ac23b9708583224a15b30c863088856d240
ahmad <eng.ahmed.anwar@gmail.com>
2022-10-02 18:04:04 +1300

    get required information about user interaction

--- index.js
+++ index.js
@@ -11,5 +11,34 @@ client.once('ready', () => {
   console.log('Ready!')
 })
 
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isChatInputCommand()) return
+
+  const { commandName } = interaction
+  console.log(JSON.stringify(interaction.member, null, 2))
+
+  if (commandName === 'list') {
+    await interaction.reply({ content: 'Pong!', ephemeral: true })
+  } else if (commandName === 'attend') {
+    const nickname =
+      interaction.member.displayName || interaction.member.nickname
+    const userId = interaction.user.id
+    const cohort = interaction.channel.parent.name
+
+    const data = {
+      cohort,
+      nickname,
+      userId,
+    }
+
+    // do something with data
+
+    await interaction.reply({
+      content: 'Kia pai te ra 😄',
+      ephemeral: true,
+    })
+  }
+})
+
 // Login to Discord with your client's token
 client.login(process.env.DISCORD_TOKEN)
